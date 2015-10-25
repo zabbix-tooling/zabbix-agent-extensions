e66d16941b44d9bfa9141d6a4cb9983ab286656f
Nils Schaeffler <nils.schaeffler@breuninger.de>
2015-10-25 15:22:29 +0100

    create zabbix monitoring using puppet state.yaml

--- agent-scripts/tools/zabbix_check_puppetstate.rb
+++ agent-scripts/tools/zabbix_check_puppetstate.rb
@@ -2,13 +2,16 @@
 
 require 'yaml'
 
-#puppet_state=YAML.load_file('/var/lib/puppet/state/last_run_summary.yaml')
+puppet_state = YAML.load_file('/var/lib/puppet/state/last_run_summary.yaml')
 
-puppet_state=YAML.load_file('/home/nschaeffler/Documents/work/git/zabbix-agent-extensions/agent-scripts/tools/foo.yaml')
+value = nil
 
-if (ARGV.lenght == 1)
-  puts "Argument: #{ARGV[0]} #{puppet_state.fetch(ARGV[0])}"
-elseif (ARGV.lenght == 2)
-  puts "Argument: #{ARGV[0]} / #{ARGV[1]} #{puppet_state.fetch(ARGV[0]).fetch(ARGV[1])}"
+ARGV.each do |arg|
+  if value == nil
+    value = puppet_state.fetch(arg)
+  else
+    value = value.fetch(arg)
+  end
 end
 
+print "#{value}"
