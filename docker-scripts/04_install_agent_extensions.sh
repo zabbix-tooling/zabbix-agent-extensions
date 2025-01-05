7ffa401c6c3e8bd5779215b4abd548877e144374
Marc Schöchlin <marc.schoechlin@uhurutec.com>
2025-01-05 12:06:24 +0100

    optimize release workflow
    - use explicity the specified version
    - combine the release process in a single job

--- docker-scripts/04_install_agent_extensions.sh
+++ docker-scripts/04_install_agent_extensions.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+set -xe
 cd /tmp/setup
 ls -l /tmp/setup
-dpkg -i *.deb
+dpkg -i "zabbix-agent-extensions_${BUILD_VERSION?The version of the extension}_all.deb"
 sed -i '~s,/var/lib/zabbix/:/sbin/nologin,/var/lib/zabbix/:/bin/bash,' /etc/passwd
